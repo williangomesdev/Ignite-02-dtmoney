14a759adc551b6f4a230f71c634a5beb936d364b
Willian Amaro Gomes <willianamaroti@gmail.com>
2021-11-30 00:17:13 -0300

    Modal Open/Close after click in new transaction button

--- src/components/Header/Index.tsx
+++ src/components/Header/Index.tsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
+import Modal from "react-modal";
 
 export function Header() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dtmoney logo" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={handleOpenNewTransactionModal}>
+          Nova transação
+        </button>
+        <Modal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Cadastrar transação</h2>
+          <input type="text" placeholder="Titulo" />
+        </Modal>
       </Content>
     </Container>
   );
